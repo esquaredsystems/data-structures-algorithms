6cd5e9c29bf83399db5efcd5965490601c4821b9
Esquared Systems <owais.a.hussain@gmail.com>
2023-08-17 14:36:29 +0500

    Adding source code for Notations and Search
    - Binary search
    - Infix to Postfix notation conversion and evaluation

---
+++ Lecture 10/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] useCases = 
+            {
+                "a+b*c", // Should output abc*+
+                "(5-2)*(3+7)", // Should output 52-37+*
+                "(((p-q)*r)+s)/t", // Should output pqr*-st/+
+            };
+            foreach (string infix in useCases)
+            {
+                string postfix = InfixToPostfix(infix);
+                Console.WriteLine($"Postfix equivalent of {infix} is {postfix}\n");
+            }
+            // CAUTION! Since we are only working on a string of single characters,
+            // therefore this function will fail if any of the operations results in a 
+            // number of more than 1 digit. For example, 34* = 12 which is 2 digits.
+            EvaluatePostfix("52-12+*");
+
+            Console.ReadKey();
+        }
+
+        public static string InfixToPostfix(string infix)
+        {
+            // Enclose the incoming string in starting and ending parentheses
+            infix = '(' + infix + ')';
+            string postfix = "";    // This will be returned
+            Stack<char> stack = new Stack<char>();  // Initialize an empty stack
+            foreach(char c in infix)
+            {
+                // Operators and starting brackets will be pushed to stack
+                if (c == '(' || c == '+' || c == '-' || c == '*' || c == '/')
+                {
+                    stack.Push(c);
+                    Console.WriteLine($"Pushing {c} to Stack.");
+                }
+                else if (c == ')')
+                {
+                    // Keep popping until next character is closing bracket
+                    while (stack.Peek() != '(') 
+                    {
+                        char top = stack.Pop();
+                        postfix += top;
+                        Console.WriteLine($"Popped {top} and added to Postfix.");
+                    }
+                }
+                // Operands are appended to postfix
+                else
+                {
+                    postfix = postfix + c;
+                }
+                Console.WriteLine($"Postfix is now {postfix}");
+            }
+            // Empty the Stack and append to postfix whatever remains
+            while (stack.Count > 0)
+            {
+                char top = stack.Pop();
+                if (top != '(' && top != ')')
+                {
+                    postfix += top;
+                }
+            }
+            return postfix;
+        }
+
+        public static void EvaluatePostfix(string postfix)
+        {
+            Stack<char> stack = new Stack<char>();  // Initialize an empty stack
+            foreach (char c in postfix)
+            {
+                if (c == '+' || c == '-' || c == '*' || c == '/')
+                {
+                    // Pop two elements and convert to integers
+                    int a = Int16.Parse(stack.Pop().ToString());
+                    int b = Int16.Parse(stack.Pop().ToString());
+                    int value = 0;
+                    // Calculate the value
+                    switch (c)
+                    {
+                        case '+':
+                            value = b + a;
+                            break;
+                        case '-':
+                            value = b - a;
+                            break;
+                        case '*':
+                            value = b * a;
+                            break;
+                        case '/':
+                            value = b / a;
+                            break;
+                    }
+                    // Push the value to stack
+                    stack.Push(value.ToString()[0]);
+                }
+                else
+                {
+                    stack.Push(c);
+                }
+            }
+            Console.WriteLine(stack.Pop());
+        }
+
+        
+    }
+}
---
+++ Lecture 9/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture_9
+{
+    internal class Program
+    {
+        static int x = 0;
+        static void Main(string[] args)
+        {
+            int[] array = { 11864, 12807, 13045, 13826, 64669, 64901, 64910, 64930, 64946, 64967, 65019, 65040, 65051, 65073, 65139, 65144, 65187 };
+
+            int random = new Random().Next(array.Length);
+            Console.WriteLine($"Searching for {array[random]}");
+            
+            SearchPartial(array, array[random]);
+            int index = SearchBinary(array, 0, array.Length, array[random]);
+            if (index >= 0)
+            {
+                Console.WriteLine($"The element was found at {index}");
+            }
+            else
+            {
+                Console.WriteLine("Not found!");
+            }
+
+            Console.ReadKey();
+        }
+
+        public static int SearchLinear(int[] array, int q)
+        {
+            for (int i = 0; i < array.Length; i++)
+            { 
+                if (array[i] == q)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        public static int SearchPartial(int[] array, int q)
+        {
+            // Get a cutoff value to split the array from
+            int cutoff = (int) Math.Ceiling((float)array.Length / 2);
+            int start = 0;  
+            int end = array.Length;
+            if (q < array[cutoff])
+            {
+                end = cutoff;
+            }
+            else
+            {
+                start = cutoff;
+            }
+            Console.WriteLine($"Iterating from {start} to {end}");
+            for (int i = start; i < end; i++)
+            {
+                if (array[i] == q)
+                {
+                    Console.WriteLine($"Found at {i}");
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        /// <summary>
+        /// Sorts an array using Binary search algorithm
+        /// </summary>
+        /// <param name="array">Sorted integer array</param>
+        /// <param name="start">starting index</param>
+        /// <param name="end">Ending index</param>
+        /// <param name="q">Search query</param>
+        /// <returns></returns>
+        public static int SearchBinary(int[] array, int start, int end, int q)
+        {
+            int difference = end - start;
+            Console.WriteLine($"The search space is from {start} to {end}");
+            if (difference > 1)
+            {
+                int cutoff = start + (int)Math.Ceiling((float)(difference) / 2);
+                if (q < array[cutoff])
+                {
+                    end = cutoff;
+                }
+                else
+                {
+                    start = cutoff;
+                }
+                return SearchBinary(array, start, end, q);
+            }
+            else if (array[start] == q)
+            {
+                return start;
+            }
+            else if (array[end] == q)
+            {
+                return end;
+            }
+            return -1;
+        }
+    }
+}
