ad1cbcd1efa31fbe9b304dcc890ebbf302c8b73a
Esquared Systems <owais.a.hussain@gmail.com>
2023-08-29 13:48:14 +0500

    Adding Merge Sort implementation

---
+++ Lecture 13/DataItem.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture_13
+{
+    internal class DataItem : IComparable<DataItem>
+    {
+        private int studentId;
+        private string fullName;
+        private float cgpa;
+
+        public DataItem() { }
+
+        public DataItem(int studentId, string fullName, float cgpa)
+        {
+            this.studentId = studentId;
+            this.fullName = fullName;
+            this.cgpa = cgpa;
+        }
+
+        public int getStudentId() {  return studentId; }
+        public string getFullName() { return fullName; }
+        public float getCGPA() {  return cgpa; }
+
+        public void setStudentId(int studentId) { this.studentId = studentId; } 
+        public void setFullName(string fullName) { this.fullName = fullName; }
+        public void setCGPA(int cgpa) { this.cgpa = cgpa; }
+
+        public override String ToString()
+        {
+            return "SID: " + studentId + ", Full Name: " + fullName + ", CGPA: " + cgpa;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is DataItem item &&
+                   obj.GetHashCode() == GetHashCode();
+        }
+
+        public override int GetHashCode()
+        {
+            int hashCode = -1808320576;
+            hashCode = hashCode * -1521134295 + studentId.GetHashCode();
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(fullName);
+            hashCode = hashCode * -1521134295 + cgpa.GetHashCode();
+            return hashCode;
+        }
+
+        public int CompareTo(DataItem other)
+        {
+            return other.studentId.CompareTo(this.studentId);
+        }
+    }
+}
---
+++ Lecture 13/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture_13
+{
+    internal class Program
+    {
+        static DataItem[] items;
+
+        static void Main(string[] args)
+        {
+            DataItem hs = new DataItem(5019, "HAFS SIDDIQUI", 3.3f);
+            DataItem am = new DataItem(4669, "ASRA MASOOD", 3.0f);
+            DataItem mh = new DataItem(5040, "ABDUL MOHIMIN", 2.6f);
+            DataItem sk = new DataItem(4930, "SHAHZAMAN KHAN", 3.2f);
+            DataItem ob = new DataItem(4901, "OMAR BAIG", 2.2f);
+            DataItem ok = new DataItem(4910, "OMAR KHAN", 2.5f);
+            DataItem jr = new DataItem(5144, "JAWAD RAZA", 2.6f);
+            items = new DataItem[] {hs, am, mh, sk, ob, ok, jr};
+            items = MergeSort(0, items.Length - 1);
+            foreach (DataItem item in items)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
+        }
+
+        static DataItem[] MergeSort(int lower, int upper)
+        {
+            if (lower < upper)
+            {
+                int mid = (lower + upper) / 2;
+                DataItem[] left = MergeSort(lower, mid);
+                DataItem[] right = MergeSort(mid + 1, upper);
+                return Merge(left, right);
+            }
+            return new DataItem[] { items[lower] };
+        }
+
+        static DataItem[] Merge(DataItem[] left, DataItem[] right)
+        {
+            DataItem[] merged = new DataItem[left.Length + right.Length];
+            int i = 0, l = 0, r = 0;
+            // Run for the items which match in length
+            while(l < left.Length && r < right.Length)
+            {
+                if (left[l].CompareTo(right[r]) > 0)
+                {
+                    merged[i] = left[l++];
+                }
+                else
+                {
+                    merged[i] = right[r++];
+                }
+                i++;
+            }
+            // Copy remaining items from left
+            while(l < left.Length)
+            {
+                merged[i++] = left[l++];
+            }
+            // Copy remaining items from right
+            while (r < right.Length)
+            {
+                merged[i++] = right[r++];
+            }
+            return merged;
+        }
+    }
+}
